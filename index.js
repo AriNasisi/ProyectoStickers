e8f427992131d26775d8f24b526e0410d2fd27f1
Ariadna Nasisi <ari.nasisi@gmail.com>
2021-10-06 22:51:44 -0300

    Agregamos titulos pagina ppal y template en js

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("navbarjs").innerHTML = `
+    <div>
+    <div class="saturn"><img src="icono2.jpg" alt="ico" id="icono"></div>
+    <a href="index.html"><div class="navbar">Juanito y sus pegatinas</div></a>
+    </div>
+    <div>
+    <a href="Catalogo.html"><div class="navbar">Catálogo</div></a>
+    <a href="personalizados.html"><div class="navbar">Stickers personalizados</div></a>
+    </div>
+    `
+
+
+document.getElementById("footerjs").innerHTML = `
+    <p> Juanito y sus pegatinas - © 2021 </p>
+    <a href="https://facebook.com"><img src="face.png" alt="" class="redes"></a>
+    <a href="https://instagram.com"><img src="insta.png" alt="" class="redes"></a>
+    `
